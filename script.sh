6236fb15f6982565b096299b29f936e00058eb67
Habib <121476164+HabibHabib92@users.noreply.github.com>
2024-05-26 10:25:55 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-echo "Current directory: $(pwd)"
-ls -l  # List files and directories in the current directory for debugging
+echo "Current directory: $(pwd)"  # Print the current directory
+ls -l                              # List the files in the current directory
+
+vename='myenv'                    # Define the virtual environment name
+
+. $vename/scripts/activate        # Activate the virtual environment
+
+pip install flask                 # Install Flask using pip
+
+python app.py                     # Run the Flask application
