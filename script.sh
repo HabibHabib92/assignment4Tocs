64b45f047072995320981407fee52506d19a3855
Habib <121476164+HabibHabib92@users.noreply.github.com>
2024-05-26 09:46:28 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-# Check if virtual environment exists, and create it if not
 venvname="env"
 if [ ! -d "$venvname" ]; then
-    echo "Creating virtual environment '$venvname'..."
     python3 -m venv "$venvname"
-    echo "Virtual environment '$venvname' created."
 fi
 
-# Activate the virtual environment
-echo "Activating virtual environment '$venvname'..."
-. "$venvname/bin/activate"
-echo "Virtual environment '$venvname' activated."
-
-# Install Flask using pip within the virtual environment
-echo "Installing Flask..."
-pip install flask
-echo "Flask installed successfully."
-
-# Run the application using python within the virtual environment
-echo "Running the application..."
-python app.py
