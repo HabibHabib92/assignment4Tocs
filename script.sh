b63ac0be6f25850f945091593ae7c414b47ebf0a
Habib <121476164+HabibHabib92@users.noreply.github.com>
2024-05-26 10:07:25 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,3 @@
 
 echo "Current directory: $(pwd)"
 ls -l  # List files and directories in the current directory for debugging
-
-# Create virtual environment
-venvname="envh"
-python3 -m venv "$venvname"
