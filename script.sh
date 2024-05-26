94cd1946a8cc666d5b30e668c6c2665c88eaa6e2
Habib <121476164+HabibHabib92@users.noreply.github.com>
2024-05-26 08:21:33 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ venvname="env"
 # Check if virtual environment exists, and create it if not
 if [ ! -d "$venvname" ]; then
     echo "Creating virtual environment '$venvname'..."
-    python -m venv "$venvname"
+    python3 -m venv "$venvname"
     echo "Virtual environment '$venvname' created."
 fi
 
